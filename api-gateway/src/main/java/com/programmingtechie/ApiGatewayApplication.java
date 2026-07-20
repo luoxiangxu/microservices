ca0e08a2f7d04c8c0e4ba2b4aea6de7b11f875bc
luoxiangxu <luoxiangxu666@gmail.com>
2026-07-20 13:27:43 +0630

    '1'

--- api-gateway/src/main/java/com/programmingtechie/ApiGatewayApplication.java
+++ api-gateway/src/main/java/com/programmingtechie/ApiGatewayApplication.java
@@ -1,10 +1,11 @@
-package com.programmingtechie.apigateway;
+package com.programmingtechie;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient  
 public class ApiGatewayApplication {
 
     public static void main(String[] args) {
